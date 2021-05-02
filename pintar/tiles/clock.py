a194f4f3cb183cb6916ea85e9cf374b5fae088a4
Charles Rumford <charlesr@tallwireless.com>
2021-05-02 19:51:25 -0400

    Working clock module

--- pintar/tiles/clock.py
+++ pintar/tiles/clock.py
@@ -1,71 +1,104 @@
-from PIL import Image, ImageDraw, ImageFont
 import arrow
-import sys
+from .tile import Tile
+from .text import generateBoundText
+from PIL import Image
+from .. import factory
 
 
-def drawText(xy, bbox, text, font, drawer, offset=0):
-    (text_x, text_y) = drawer.textsize(text, font=font)
-    text_x = (bbox[0] - text_x) / 2 + xy[0]
-    drawer.text((text_x, xy[1] + offset), text, font=font)
-    return (xy[0], xy[1] + text_y + offset)
+class Clock(Tile):
+    """
+    This tile will produce a digital style clock for each city and timezone
+    given in the cities section of the configuration.
+
+    Optional Configuration Parameters:
+        alignment:
+            How the clocks should be aligned in the space. Valid entries are
+            horizonal and vertical. Default horizonal.
+        cities:
+            A dictionary of cities and their timezones. Key is the city, and
+            the value is the timezone. Default is the configured timezone for
+            the running device.
+    """
+
+    tile_type = "clock"
+
+    def __init__(self, user_config: dict):
+        # setup the default configuration
+        config = {"alignment": "horizonal", "cities": {"Local": "local"}}
+
+        config.update(user_config)
+        print(config)
+        super(Clock, self).__init__(config)
+
+    def generateImage(self) -> Image:
+        """
+        generate all of the clocks and pass back the image!
+
+        """
+        num_cities = len(self.config["cities"])
+        layout = {"city": 60, "time": 150, "date": 50}
+        if self.config["alignment"] == "vertical":
+            width = int(self.config["size_x"])
+            height = int(self.config["size_y"] / num_cities)
+            x_add = 0
+            y_add = height
+        elif self.config["alignment"] == "horizonal":
+            width = int(self.config["size_x"] / num_cities)
+            height = int(self.config["size_y"])
+            x_add = width
+            y_add = 0
+        else:
+            raise ValueError("Invaild configuration option for alignment")
+
+        current = (0, 0)
+        total_cities = len(self.config["cities"])
+        for (count, (city, timezone)) in enumerate(self.config["cities"].items()):
+            img = self.makeClock(city, timezone, width, height, layout)
+            self.image.paste(img, current)
+            current = (current[0] + x_add, current[1] + y_add)
+
+            if count < (total_cities - 1):
+                line_length = 5
+                x_interval = int(width / line_length)
+                start_x = x_interval
+                end_x = x_interval * (line_length - 1)
+                y = current[1]
+                self.drawer.line((start_x, y, end_x, y), width=5)
+
+        return self.image
+
+    def makeClock(
+        self, city: str, timezone: str, width: int, height: int, layout: dict
+    ) -> Image:
+        img, drawer = factory.imageFactory(width, height)
+        time = arrow.utcnow().to(timezone)
+
+        city_text = generateBoundText(
+            city,
+            factory.FontFactory(layout["city"]),
+        )
+        time_text = generateBoundText(
+            time.format("HH:mm"),
+            factory.FontFactory(layout["time"]),
+        )
+        date_text = generateBoundText(
+            time.format("YYYY-MM-DD"),
+            factory.FontFactory(layout["date"]),
+        )
+        margin = 40
+        total_y = city_text.height + time_text.height + date_text.height + margin * 2
+        start_y = int((height - total_y) / 2)
+        start_x = int((width - city_text.width) / 2)
+        img.paste(city_text, (start_x, start_y))
+        start_x = int((width - time_text.width) / 2)
+        img.paste(time_text, (start_x, start_y + city_text.height + margin))
+        start_x = int((width - date_text.width) / 2)
+        img.paste(
+            date_text,
+            (start_x, start_y + city_text.height + time_text.height + margin * 2),
+        )
+        return img
 
 
-def makeClock(city, timezone, height=250, width=355):
-    sys.stderr.write(f"{city}:{timezone}\n")
-    fonts = {
-        "time": ImageFont.truetype(
-            "/home/charlesr/.local/share/fonts/NerdFonts/Noto Sans Mono Condensed Nerd Font Complete.ttf",
-            130,
-        ),
-        "city": ImageFont.truetype(
-            "/usr/share/fonts/truetype/tlwg/Laksaman-Bold.ttf", 40
-        ),
-        "date": ImageFont.truetype(
-            "/usr/share/fonts/truetype/tlwg/Laksaman-Bold.ttf", 40
-        ),
-    }
-    img = Image.new("1", (width, height), (1))
-    time = arrow.utcnow().to(timezone)
-    drawer = ImageDraw.Draw(img)
-    start = (0, 0)
-    bbox = (width, height)
-    current = start
-
-    current = drawText(current, bbox, city, fonts["city"], drawer)
-    current = drawText(current, bbox, time.format("HH:mm"), fonts["time"], drawer, -35)
-    current = drawText(current, bbox, time.format("YYYY/MM/DD"), fonts["date"], drawer)
-    return img
-
-
-primary = Image.new("L", (1872, 1404), ("white"))
-drawer = ImageDraw.Draw(primary)
-
-cities = {"Philadelphia": "US/Eastern", "Chicago": "US/Central", "UTC": "UTC"}
-num_cities = len(cities)
-current = (0, 0)
-for city in cities:
-    img = makeClock(city, cities[city], width=int(primary.getbbox()[2] / num_cities))
-    primary.paste(img, current)
-    #    current = (current[0] img.getbbox()[2], current[1] + img.getbox())
-    drawer.line(
-        (
-            primary.getbbox()[2] / num_cities,
-            0 + 40,
-            primary.getbbox()[2] / num_cities,
-            current[1] - 40,
-        ),
-        width=2,
-    )
-line_factor = 10
-
-drawer.line(
-    (
-        int(primary.getbbox()[2] / line_factor),
-        img.getbbox()[3],
-        int(primary.getbbox()[2] / line_factor) * (line_factor - 1),
-        img.getbbox()[3],
-    ),
-    width=2,
-)
-
-primary.save(sys.stdout.buffer, format="BMP")
+def getClass():
+    return Clock
