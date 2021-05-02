9c261c7ca3df6a4de6e0640a2e6cb1b785418b27
Charles Rumford <charlesr@tallwireless.com>
2021-05-02 19:50:57 -0400

    Some simple helper functions

---
+++ pintar/helpers.py
@@ -0,0 +1,12 @@
+from PIL import Image, ImageChops
+
+
+def trim(im):
+    bg = Image.new(im.mode, im.size, im.getpixel((0, 0)))
+    diff = ImageChops.difference(im, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    # Bounding box given as a 4-tuple defining the left, upper, right, and lower pixel coordinates.
+    # If the image is completely empty, this method returns None.
+    bbox = diff.getbbox()
+    if bbox:
+        return im.crop(bbox)
